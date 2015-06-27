14ae523df0408208f61f99912f5a814c8853a62b
Abdoulaye Balde <abdoulisme@gmail.com>
2015-06-26 23:33:41 -0400

    Major changes to Game Page to Include layout with buttons

--- app/src/main/java/com/wordpress/logitopia/jun20/GamePage.java
+++ app/src/main/java/com/wordpress/logitopia/jun20/GamePage.java
@@ -2,6 +2,10 @@ package com.wordpress.logitopia.jun20;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Phenomeno on 6/26/15.
@@ -11,5 +15,27 @@ public class GamePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_game);
+
+        Button button_one, button_two, button_three;
+
+        button_one = (Button)findViewById(R.id.button1);
+
+        button_two = (Button)findViewById(R.id.button2);
+
+        button_three = (Button)findViewById(R.id.button3);
+
+
+
     }
+
+
+    public void option_one_Click(View view) {
+    }
+
+    public void option_two_Click(View view) {
+    }
+
+    public void option_three_Click(View view) {
+    }
+
 }
